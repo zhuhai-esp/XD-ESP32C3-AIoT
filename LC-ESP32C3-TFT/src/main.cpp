fa0ca5591bf87bb58ec04ea900100d542b00cd3f
huzongyao <huzongyao@tangbull.com>
2024-04-20 12:02:33 +0800

    add hello project

---
+++ LC-ESP32C3-TFT/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft;
+
+void setup() {
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  pinMode(TFT_BL, OUTPUT);
+  analogWriteResolution(10);
+  analogWrite(TFT_BL, 200);
+
+  tft.setTextFont(4);
+  tft.drawString("Hello World!!", 0, 0);
+  tft.drawRoundRect(20, 30, 200, 100, 12, TFT_YELLOW);
+  tft.drawCircle(150, 150, 40, TFT_RED);
+  tft.drawRect(60, 60, 30, 40, TFT_BLUE);
+}
+
+void loop() {}
